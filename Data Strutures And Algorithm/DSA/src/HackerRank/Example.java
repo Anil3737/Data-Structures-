2f4b1a0a4a0e2373638d8f06607ed6219ac94453
Jada Chiranjevi Anil <jadachiranjevianil340@gmail.com>
2026-04-09 21:27:22 +0530

    initial commit

---
+++ Data Strutures And Algorithm/DSA/src/HackerRank/Example.java
@@ -0,0 +1,8 @@
+package HackerRank;
+
+public class Example  {
+		public static void main(String[] args) {
+			System.out.println("hello world");
+		}
+	
+}
---
+++ Data Strutures And Algorithm/DSA/src/HackerRank/LogicHack_41.java
@@ -0,0 +1,10 @@
+package HackerRank;
+import java.util.*;
+public class LogicHack_41 {
+	public static void input() {
+		
+	}
+	public static void main(String[] args) {
+		input();
+	}
+}
