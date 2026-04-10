5b55f781b7bd445bb8b393d82b5bfa1d6d49e75d
Jada Chiranjevi Anil <jadachiranjevianil340@gmail.com>
2026-04-10 21:46:34 +0530

    Leetcode Problems Day 2

---
+++ Data Strutures And Algorithm/DSA/src/Leetcode/MaxSubArray.java
@@ -0,0 +1,27 @@
+package Leetcode;
+import java.util.*;
+public class MaxSubArray {
+	public static int maxSubArray(int[] nums) {
+		int maxSum = nums[0];
+		int currentSum = nums[0];
+		for(int i=1;i<nums.length;i++) {
+			currentSum = Math.max(nums[i], currentSum+nums[i]);
+			maxSum = Math.max(maxSum, currentSum);
+		}
+		return maxSum;
+		
+	}
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Array Size :: ");
+		int size = scanner.nextInt();
+		System.out.println("Enter "+size +" Elements :: ");
+		int [] array = new int[size];
+		for(int i=0;i<size;i++) {
+			array[i] = scanner.nextInt();
+		}
+		int result = maxSubArray(array);
+		System.out.println(result);
+	}
+
+}
